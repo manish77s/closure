a230556a619d68a3bcca3107df800db036382a0a
manish77s <69430012+manish77s@users.noreply.github.com>
2023-07-17 00:58:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+//     function counter(){
+//     var counter = 0;
+
+//     function IncreaseCounter(){
+//         return counter += 1;
+//     };
+
+//     return IncreaseCounter;
+// }
+
+
+// var counter = counter();
+// alert(counter());
+// alert(counter());
+// alert(counter());
+// alert(counter());
+
+
+// let count = 0;
+// (function () {
+//   if (count === 0) {
+//     let count = 1;
+//     console.log(count); // What is logged?
+//   }
+//   console.log(count); // What is logged?
+// })();
+
+//  for(i=0;i<3;i++){_
+//     setTimeout(function log(){
+//         console.log(i)
+//     },1000)
+// }
